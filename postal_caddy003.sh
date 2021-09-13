456c7e39fa905bea53367b58f24fe45a41e5c062
layen67 <lkbcontact@gmail.com>
2021-09-13 18:20:10 +0100

    Update postal_caddy003.sh

--- postal_caddy003.sh
+++ postal_caddy003.sh
@@ -111,7 +111,8 @@ apt install git curl jq;
 git clone https://postalserver.io/start/install /opt/postal/install;
 sudo ln -s /opt/postal/install/bin/postal /usr/bin/postal;
 apt-get update;apt-get install -y docker.io;
-curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose;chmod +x /usr/local/bin/docker-compose;
+curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose;
+chmod +x /usr/local/bin/docker-compose;
 
 docker run -d \
    --name postal-mariadb \
