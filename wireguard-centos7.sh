4bc0ceed74f2601ad89247165b8f93e2495cc7d3
layen67 <lkbcontact@gmail.com>
2022-05-24 14:14:53 +0200

    Update wireguard-centos7.sh

--- wireguard-centos7.sh
+++ wireguard-centos7.sh
@@ -5,6 +5,6 @@ sudo yum update -y;
 sudo yum install -y epel-release https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm;
 sudo yum install -y yum-plugin-elrepo;
 sudo yum install -y kmod-wireguard wireguard-tools;
+sudo yum install nano -y;
 touch /etc/wireguard/wg0.conf;
 sudo systemctl enable wg-quick@wg0;
-
